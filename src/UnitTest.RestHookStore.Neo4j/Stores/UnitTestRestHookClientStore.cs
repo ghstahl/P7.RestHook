091da4ee09cb3f6e26c60a54a9b59755b5b7335a
ghstahl@gmail.com <ghstahl@gmail.com>
2018-07-11 15:40:47 -0700

    initial

---
+++ src/UnitTest.RestHookStore.Neo4j/Stores/UnitTestRestHookClientStore.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P7.RestHook.ClientManagement;
+
+namespace UnitTest.RestHookStore.Neo4j.Stores
+{
+    [TestClass]
+    public class UnitTestRestHookClientStore : UnitTest.RestHookStore.Core.Stores.UnitTestRestHookClientStore
+    {
+        public UnitTestRestHookClientStore() :
+            base(
+                HostContainer.ServiceProvider.GetService<IRestHookClientManagementStoreTest>(),
+                HostContainer.ServiceProvider.GetService<IRestHookClientManagementStore>()
+            )
+        {
+        }
+    }
+}
